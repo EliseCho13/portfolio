dcb76e77cd094028d327923dbb7500a364c67368
EliseCho13 <dbwlspdlqj@naver.com>
2023-10-28 02:45:30 +0900

    feat: added pie chart

--- src/containers/skillProgress/skillProgress.js
+++ src/containers/skillProgress/skillProgress.js
@@ -4,8 +4,38 @@ import {illustration, techStack} from "../../portfolio";
 import {Fade} from "react-reveal";
 import Build from "../../assets/lottie/build";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import {Chart as ChartJS, ArcElement, Tooltip, Legend} from "chart.js";
+import {Pie} from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function StackProgress() {
+  const data = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)"
+        ],
+        borderWidth: 1
+      }
+    ]
+  };
   if (techStack.viewSkillBars) {
     return (
       <Fade bottom duration={1000} distance="20px">
@@ -25,6 +55,9 @@ export default function StackProgress() {
                 </div>
               );
             })}
+          </div>
+          <div className="pie-container">
+            <Pie data={data} />
           </div>
 
           <div className="skills-image">
